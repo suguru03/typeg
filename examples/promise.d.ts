a44bd7c5dd0d26ad1fecc413cb9517627394ae35
Suguru Motegi <suguru.motegi@gmail.com>
2020-04-13 23:29:49 -0700

    chore(package): upadte prettier-hook and fix ast issue

--- examples/promise.d.ts
+++ examples/promise.d.ts
@@ -1,4 +1,7 @@
 class Promise {
   @Times(10, 'T', { args: { values: 'arrayMulti' }, returnType: 'arrayMulti' })
   all<T>(values: [T | PromiseLike<T>]): Promise<[T]>;
+
+  @Times(5, 'E', { args: { filter: 'multi' } })
+  catch<T, E>(filter: Aigle.CatchFilter<E>, onReject: (error: E) => Aigle.ReturnType<T>): Aigle<T | R>;
 }
